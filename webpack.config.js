3743258d28b31c7b35a4bd490e33a40d16a98b09
bud93411 <bud93411@gmail.com>
2019-07-04 17:07:50 +0300

    Adding webpack and babel configurations

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		publicPath: path.join(__dirname, 'dist'),
+		filename: "[name].js"
+	},
+	plugins: [
+		new HtmlWebPackPlugin({template: "./src/index.html", filename: "./index.html"}),
+		new webpack.HotModuleReplacementPlugin(),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			}, {
+				test: /\.s?css$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			}
+		]
+	}
+};

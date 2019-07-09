9a34e3d7808ea571aa64889c461310a909571b29
bud93411 <bud93411@gmail.com>
2019-07-09 11:20:48 +0300

    Updating CSS files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles.scss';
-const Index = () => {
-	return <div>Welcome to PhoneBook!</div>;
-};
-ReactDOM.render(<Index/>, document.getElementById('root'));
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import {BrowserRouter, Route} from 'react-router';
+
+import configureStore from './store/configureStore';
+import App from './containers/app/App';
+import ErrorBoundary from './components/errorBoundary/ErrorBoundary';
+
+const store = configureStore();
+
+render(
+	<ErrorBoundary>
+	   <Provider store= { store }>
+		   <App/>
+	   </Provider>
+   	</ErrorBoundary>,
+	document.getElementById('root')
+);

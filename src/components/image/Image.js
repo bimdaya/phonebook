d856183f2b410639380978d93254976a7687924a
bud93411 <bud93411@gmail.com>
2019-07-09 12:48:17 +0300

    Updating image component as a common component

---
+++ src/components/image/Image.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+	Icon,
+	Image as Img
+} from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+
+import './Image.css'
+
+const Image = ({imageSrc, imageSize, iconSize}) => {
+	return (
+		<>
+			{
+				imageSrc ?
+					<Img
+						centered
+						circular
+						size={imageSize}
+						src={imageSrc}/> :
+					<Icon
+						className='profile_icon'
+						name='user circle'
+						size={iconSize}/>
+			}
+		</>
+	);
+};
+
+Image.defaultProps = {
+	imageSize: 'medium',
+	iconSize: 'massive'
+}
+
+Image.propTypes = {
+	imageSrc: PropTypes.string,
+	imageSize: PropTypes.string,
+	iconSize: PropTypes.string
+}
+
+export default Image;

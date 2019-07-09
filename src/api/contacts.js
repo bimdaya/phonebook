36d9c7147098e75058964048a3e5adb92df8f725
bud93411 <bud93411@gmail.com>
2019-07-09 11:22:37 +0300

    Adding async API calls

---
+++ src/api/contacts.js
@@ -0,0 +1,13 @@
+import axios from '../config/axios';
+
+/**
+ * Get contacts with details from API
+ * @return {object} array of contact objects
+ * @throw {Error}
+ */
+export const getAllContacts = () => axios.get('/contacts')
+	.then(response => response.data)
+	.catch((error) => {
+		throw new Error(`An error occured while fetching
+			contacts from the server. ${error}`);
+	});

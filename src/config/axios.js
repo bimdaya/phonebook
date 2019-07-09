8c2325ae891efc3c417cda9d9a1e13ddb173159b
bud93411 <bud93411@gmail.com>
2019-07-09 13:27:12 +0300

    Fixing linting issues

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-import {GET_CONTACTS_BASE_URL} from '../constants/api';
+import { GET_CONTACTS_BASE_URL } from '../constants/api';
 
-const instance = axios.create({baseURL: GET_CONTACTS_BASE_URL});
+const instance = axios.create({ baseURL: GET_CONTACTS_BASE_URL });
 
 export default instance;

9351eb8b5c87a633e59ca71b3044b77246711d99
bud93411 <bud93411@gmail.com>
2019-07-09 11:28:38 +0300

    Configuring regex and validations

---
+++ src/utils/regex.js
@@ -0,0 +1,31 @@
+/**
+ * Filter list items using the query
+ * @param {string} query search query
+ * @param {string} list object list
+ * @param {string} filters object keys
+ * @return {object} filtered objects
+ */
+export const searchQuery = (query, list, filters) => Array.from(list).filter((item) => {
+	const regex = new RegExp(`^${query}`, 'i');
+
+	for (const filter of filters) {
+		if (regex.test(item[filter])) {
+			return true;
+		}
+	}
+	return false;
+});
+
+/**
+ * Validate input string to only accept letters
+ * @param {string} value input value
+ * @return {boolean} valid/invalid
+ */
+export const validateInputString = (value) => {
+	const regex = new RegExp(/^[a-zA-Z\s]*$/);
+
+	if (regex.test(value)) {
+		return true;
+	}
+	return false;
+};
---
+++ src/utils/validate.js
@@ -0,0 +1,37 @@
+import { validateInputString } from './regex';
+
+/**
+ * Validate input string to only accept letters
+ * @param {string} value input value
+ * @return {boolean} valid/invalid
+ */
+export const validateSearchInput = (value, keyCode) => {
+	if (validateInputString(value) || !value) {
+		return true;
+	}
+
+	// includes space, delete, arrow keys, shift, etc.
+	if (keyCode >= 8 && keyCode <= 46) {
+		return true;
+	}
+
+	return false;
+};
+
+/**
+ * Return full name of a user
+ * @param {string} firstName first name
+ * @param {string} lastName last name
+ * @return {string} fullname
+ */
+export const getDisplayName = (firstName, lastName) => {
+	if (firstName) {
+		return `${firstName} ${lastName}`;
+	}
+
+	if (lastName) {
+		return lastName;
+	}
+
+	return 'Anonymous';
+};
